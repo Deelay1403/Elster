1ccf4a286e0ac5b3dba2fa06b36a7ff5b9ccdd85
deelay1403 <PatrixH2O1400.3>
2016-10-30 00:47:51 +0200

    Upgrade && Save - New File with class

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Elster_Git',
+    version='',
+    packages=['GitHub'],
+    url='',
+    license='',
+    author='root',
+    author_email='',
+    description=''
+)
