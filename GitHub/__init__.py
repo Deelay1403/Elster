8f730a6f3bd6224ae54ffd60fd8b44a2fb72f9fe
deelay1403 <PatrixH2O1400.3>
2016-11-10 10:38:18 +0100

    Upgrade && Save - Working with generator - Continue

--- GitHub/__init__.py
+++ GitHub/__init__.py
@@ -1,3 +1,10 @@
 import arduino_universal_oriented
+import generateScene
+import gtk
 if __name__ == "__main__":
     arduino_universal_oriented
+    generateScene
+    gtk.main()
+'''Na razie nie dziala, program nalezy uruchamiac interpreterem z osobna arduino_universal_oriented oraz generateScene
+    W razie problemu należy doinstalować brakujące biblioteki(interpreter powie jakie)
+'''
