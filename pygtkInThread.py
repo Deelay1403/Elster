5c93497916f39e9395d5a692d0f729f0f28d9683
rrakso <oskarrro90@gmail.com>
2017-03-29 00:03:40 +0200

    ffrgbfhngjhtygfdszdtvcfjvndbhgvy
    `uias\sgrka\dmkv,

---
+++ pygtkInThread.py
@@ -0,0 +1,77 @@
+import threading
+import time
+import gobject
+import gtk
+
+gobject.threads_init()
+
+class MyThread(threading.Thread):
+ def __init__(self, label):
+     super(MyThread, self).__init__()
+     self.label = label
+     self.quit = False
+
+ def update_label(self, counter):
+     self.label.set_text("Counter: %i" % counter)
+     return False
+
+ def run(self):
+     counter = 0
+     while not self.quit:
+         counter += 1
+         gobject.idle_add(self.update_label, counter)
+         time.sleep(0.001)
+
+w = gtk.Window()
+l = gtk.Label()
+w.add(l)
+w.show_all()
+w.connect("destroy", lambda _: gtk.main_quit())
+t = MyThread(l)
+t.start()
+
+gtk.main()
+t.quit = True
+
+# import threading
+# import time
+# import gobject
+# import gtk
+
+# gobject.threads_init()
+
+# class licz:
+#  def __init__(self):
+#      self.quit = False
+#      w = gtk.Window()
+#      l = gtk.Label()
+#      w.add(l)
+#      w.show_all()
+#      self.label = l
+#      w.connect("destroy", lambda _: gtk.main_quit())
+
+#  def update_label(self, counter):
+#      self.label.set_text("Counter: %i" % counter)
+#      return False
+
+#  def run(self):
+#      counter = 0
+#      while not self.quit:
+#          counter += 1
+#          gobject.idle_add(self.update_label, counter)
+#          time.sleep(0.0000001)
+
+#  def startT(self):
+#     gtk.threads_enter()
+#     gtk.main()
+#     gtk.threads_leave()
+
+# def xD():
+#     global t
+#     t = licz()
+#     t.startT()
+#     print "DZIALA!"
+
+# popcorn = threading.Thread(target=xD, name="Battery").start()
+
+# print "chujjj"
