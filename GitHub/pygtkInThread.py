789315c95a43dd8e30fe122845dedf7949cad204
deelay1403 <PatrixH2O1400.3>
2017-03-29 12:50:21 +0200

    próba battery in thread

--- GitHub/pygtkInThread.py
+++ GitHub/pygtkInThread.py
@@ -6,10 +6,14 @@ import gtk
 gobject.threads_init()
 
 class MyThread(threading.Thread):
- def __init__(self, label):
+ def __init__(self):
      super(MyThread, self).__init__()
-     self.label = label
+     w = gtk.Window()
+     self.label = gtk.Label()
      self.quit = False
+     w.add(self.label)
+     w.connect("destroy", lambda _: gtk.main_quit())
+     w.show_all()
 
  def update_label(self, counter):
      self.label.set_text("Counter: %i" % counter)
@@ -20,18 +24,56 @@ class MyThread(threading.Thread):
      while not self.quit:
          counter += 1
          gobject.idle_add(self.update_label, counter)
-         time.sleep(0.001)
+         self.pisz()
+         time.sleep(0.00001)
 
-w = gtk.Window()
-l = gtk.Label()
-w.add(l)
-w.show_all()
-w.connect("destroy", lambda _: gtk.main_quit())
-t = MyThread(l)
+ def pisz(self):
+     print "KUPA"
+
+t = MyThread()
 t.start()
 
 gtk.main()
 t.quit = True
+
+# import threading
+# import time
+# import gobject
+# import gtk
+#
+# gobject.threads_init()
+#
+# class MyThread(threading.Thread):
+#  def __init__(self, label):
+#      super(MyThread, self).__init__()
+#      self.label = label
+#      self.quit = False
+#
+#  def update_label(self, counter):
+#      self.label.set_text("Counter: %i" % counter)
+#      return False
+#
+#  def run(self):
+#      counter = 0
+#      while not self.quit:
+#          counter += 1
+#          gobject.idle_add(self.update_label, counter)
+#          self.pisz()
+#          time.sleep(0.00001)
+#
+#  def pisz(self):
+#      print "KUPA"
+#
+# w = gtk.Window()
+# l = gtk.Label()
+# w.add(l)
+# w.show_all()
+# w.connect("destroy", lambda _: gtk.main_quit())
+# t = MyThread(l)
+# t.start()
+#
+# gtk.main()
+# t.quit = True
 
 # import threading
 # import time
